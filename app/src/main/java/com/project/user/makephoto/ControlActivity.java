e3232a8eecc2329d0f4179caad0cd85086b41c55
knockleopard <knockleopard@gmail.com>
2017-08-27 07:56:10 +0900

    Modify image with one external display

--- app/src/main/java/com/project/user/makephoto/ControlActivity.java
+++ app/src/main/java/com/project/user/makephoto/ControlActivity.java
@@ -1,19 +1,27 @@
 package com.project.user.makephoto;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.hardware.display.DisplayManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 
 /**
@@ -21,22 +29,38 @@ import java.io.IOException;
  */
 
 public class ControlActivity extends AppCompatActivity {
+    static ControlActivity controlActivity;
+    public static Context mContext;
     SeekBar left_luminosity;
     SeekBar left_saturation;
-    ImageView selected_image;
+    public static ImageView selected_image;
     Bitmap image_bitmap;
     int current_progress_l;
     int current_progress_s;
     final int REQ_CODE_SELECT_IMAGE=100;
 
+//    public static ImageView selected_image_static;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        controlActivity = this;
+        mContext = this;
+
         setContentView(R.layout.sub_control);
 
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
+
+//        selected_image_static = selected_image;
+
+      //  qq();
+
+
+    }
+/*
+    public void qq(){
 
         left_saturation = (SeekBar) findViewById(R.id.left_saturation);
         left_saturation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
@@ -57,24 +81,93 @@ public class ControlActivity extends AppCompatActivity {
         });
         left_luminosity = (SeekBar) findViewById(R.id.left_luminosity);
         left_luminosity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-           @Override
+            @Override
             public void onStartTrackingTouch(SeekBar seekBar){
-           }
-           @Override
+            }
+            @Override
             public void onStopTrackingTouch(SeekBar seekBar){
-               //0~100  default :  50
-               selected_image.setImageBitmap
-                       (SetBrightness
-                               (image_bitmap, current_progress_l));
+                //0~100  default :  50
+                selected_image.setImageBitmap
+                        (SetBrightness
+                                (image_bitmap, current_progress_l));
 
 
-           }
-           @Override
+            }
+            @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
                 current_progress_l=progress;
-           }
+            }
         });
     }
+*/
+    public void qq(SeekBar left_saturation, SeekBar left_luminosity){
+
+       //left_saturation = (SeekBar) findViewById(R.id.left_saturation);
+        left_saturation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){
+                //0~100  default :  50
+                selected_image.setImageBitmap(SetSaturation(image_bitmap, current_progress_s));
+
+
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                current_progress_s=progress;
+            }
+        });
+        //left_luminosity = (SeekBar) findViewById(R.id.left_luminosity);
+        left_luminosity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){
+                //0~100  default :  50
+                selected_image.setImageBitmap
+                        (SetBrightness
+                                (image_bitmap, current_progress_l));
+
+
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                current_progress_l=progress;
+            }
+        });
+    }
+
+
+    public void autoActivityChange(){
+
+
+        ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+        int i = controlActivity.getTaskId();
+        DisplayManager mDisplayManager = (DisplayManager)getSystemService(Context.DISPLAY_SERVICE);
+        // enumerate the displays
+        Display[] presentationDisplays = mDisplayManager.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
+        am.setExternalDisplay(i, presentationDisplays[0], am.SET_EXTERNAL_DISPLAY_AND_STAY);
+
+
+
+
+        ((MainActivity)MainActivity.mContext).setDefaultDisplay();
+
+
+        //moveTaskToBack(true);
+        //    List<ActivityManager.RunningTaskInfo> Info = am.getRunningTasks(1);
+        //    ComponentName topActivity = Info.get(0).topActivity;
+        //    String name = topActivity.getPackageName();
+        //  am.setExternalDisplay(data, ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay(), am.SET_EXTERNAL_DISPLAY_AND_GO_HOME);
+
+        //finish();
+    }
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -103,6 +196,17 @@ public class ControlActivity extends AppCompatActivity {
                 }
             }
         }
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                autoActivityChange();
+                //여기에 딜레이 후 시작할 작업들을 입력
+            }
+        }, 100);// 0.5초 정도 딜레이를 준 후 시작
+
     }
 
 
--- app/src/main/java/com/project/user/makephoto/MainActivity.java
+++ app/src/main/java/com/project/user/makephoto/MainActivity.java
@@ -1,27 +1,43 @@
 package com.project.user.makephoto;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.hardware.display.DisplayManager;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
-
+    static MainActivity mainActivity;
+    public static Context mContext;
+    SeekBar left_luminosity;
+    SeekBar left_saturation;
+   // ImageView selected_image;
+    Bitmap image_bitmap;
+    int current_progress_l;
+    int current_progress_s;
+    final int REQ_CODE_SELECT_IMAGE=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainActivity = this;
+        mContext = this;
         setContentView(R.layout.activity_main);
 
         Button button_select = (Button) findViewById(R.id.button);
@@ -36,7 +52,67 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent2);
             }
         });
+        qq();
 
     }
+    public void qq(){
 
+        left_saturation = (SeekBar) findViewById(R.id.left_saturation);
+        left_saturation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){
+                //0~100  default :  50
+                ((ControlActivity)ControlActivity.mContext).qq(left_saturation, left_luminosity);
+               // ControlActivity.selected_image.setImageBitmap(SetSaturation(image_bitmap, current_progress_s));
+
+
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                current_progress_s=progress;
+            }
+        });
+        left_luminosity = (SeekBar) findViewById(R.id.left_luminosity);
+        left_luminosity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar){
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){
+                //0~100  default :  50
+                ((ControlActivity)ControlActivity.mContext).qq(left_saturation, left_luminosity);
+                //selected_image.setImageBitmap(SetBrightness(image_bitmap, current_progress_l));
+
+
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                current_progress_l=progress;
+            }
+        });
+    }
+
+    public void setDefaultDisplay(){
+
+        Intent intent2 = new Intent();
+           intent2.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+           intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent2.setComponent(new ComponentName("com.project.user.makephoto", "com.project.user.makephoto.MainActivity"));
+        startActivity(intent2);
+
+
+        ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+        int i = mainActivity.getTaskId();
+        DisplayManager mDisplayManager = (DisplayManager)getSystemService(Context.DISPLAY_SERVICE);
+        // enumerate the displays
+        Display[] presentationDisplays = mDisplayManager.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
+        //am.setExternalDisplay(i, getWindowManager().getDefaultDisplay(), am.SET_EXTERNAL_DISPLAY_AND_STAY);
+        //moveTaskToBack(true);
+
+    }
 }
+
+
